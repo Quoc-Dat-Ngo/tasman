8a2f64e4fb895f531bb0da166990c77224d60f63
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-21 20:35:28 +1100

    Apply router middleware to multiple endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import express from 'express';
+import { studentsRouter } from './routes/students';
+import { coursesRouter } from './routes/courses';
+import { instructorsRouter } from './routes/instructors';
+import { majorsRouter } from './routes/majors';
+import { departmentsRouter } from './routes/deparments';
 
 const app = express();
 
@@ -6,5 +11,13 @@ const app = express();
 app.get('/', (req, res) => {
   res.status(200).send('Server is still alive');
 });
+
+// Middleware
+app.use(express.json());
+app.use('/api/v1/students', studentsRouter);
+app.use('/api/v1/courses', coursesRouter);
+app.use('/api/v1/instructors', instructorsRouter);
+app.use('/api/v1/majors', majorsRouter);
+app.use('/api/v1/departments', departmentsRouter);
 
 export default app;
