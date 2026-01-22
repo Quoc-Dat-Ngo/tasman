b9c9682d118dea10a5d405cc4096c4f74acb28bf
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-23 00:39:13 +1100

    Remove redundant console logs

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,27 @@
-import express from 'express';
-import { studentsRouter } from './routes/students';
-import { coursesRouter } from './routes/courses';
-import { instructorsRouter } from './routes/instructors';
-import { majorsRouter } from './routes/majors';
-import { departmentsRouter } from './routes/deparments';
+import express from "express";
+import morgan from "morgan";
+import { studentsRouter } from "./routes/students";
+import { coursesRouter } from "./routes/courses";
+import { instructorsRouter } from "./routes/instructors";
+import { majorsRouter } from "./routes/majors";
+import { departmentsRouter } from "./routes/deparments";
 
 const app = express();
 
 // Sanity check
-app.get('/', (req, res) => {
-  res.status(200).send('Server is still alive');
+app.get("/", (req, res) => {
+  res.status(200).send("Server is still alive");
 });
 
 // Middleware
 app.use(express.json());
-app.use('/api/v1/students', studentsRouter);
-app.use('/api/v1/courses', coursesRouter);
-app.use('/api/v1/instructors', instructorsRouter);
-app.use('/api/v1/majors', majorsRouter);
-app.use('/api/v1/departments', departmentsRouter);
+app.use(morgan("dev"));
+
+// Routing
+app.use("/api/v1/students", studentsRouter);
+app.use("/api/v1/courses", coursesRouter);
+app.use("/api/v1/instructors", instructorsRouter);
+app.use("/api/v1/majors", majorsRouter);
+app.use("/api/v1/departments", departmentsRouter);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import app from './app';
-import { initDb } from './database/init';
-import { updateDb } from './database/update';
+import app from "./app";
+import { initDb } from "./database/init";
 
 // Initialise database on start-up
 initDb();
-updateDb();
 
-const PORT = 3000;
+const PORT = 3004;
 app.listen(PORT, () => {
   console.log(`Server listen on port ${PORT}...`);
 });
