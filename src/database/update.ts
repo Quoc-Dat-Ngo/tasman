cc58f86ef1ea384ddf9455da65439cfadf32b959
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-23 22:47:43 +1100

    Reset table courses for inserting purpose

--- src/database/update.ts
+++ src/database/update.ts
@@ -4,4 +4,8 @@ export const updateDb = async (): Promise<void> => {
   await pool.query(
     "ALTER TABLE majors ADD IF NOT EXISTS department_id INTEGER NOT NULL REFERENCES departments(department_id)",
   );
+  // await pool.query("TRUNCATE TABLE courses RESTART IDENTITY CASCADE;");
+  // await pool.query(
+  //   "ALTER TABLE courses ALTER COLUMN course_code TYPE VARCHAR(16);",
+  // );
 };
