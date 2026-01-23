1abdaeea48a11909035fbf68126e19be37db27f9
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-23 18:23:05 +1100

    Work on generating mass test data; controller function for student enrollemnts

--- src/controllers/studentsController.ts
+++ src/controllers/studentsController.ts
@@ -1,30 +1,130 @@
 import type { Request, Response, NextFunction } from "express";
-
-export interface Student {
-  student_id: number;
-  first_name: string;
-  last_name: string;
-  dob: Date;
-  gender: "M" | "F";
-}
+import type { Student } from "../types/";
+import {
+  studentList,
+  addStudent,
+  studentInfo,
+  updateInfo,
+  removeInfo,
+  registerCourse,
+  getCourse,
+  getMajor,
+} from "../services/studentsService";
 
 const getAllStudent = async (
   req: Request,
   res: Response,
   next: NextFunction,
-): Promise<Student[]> | never => {
+): Promise<void> => {
   try {
-    const data = await 
+    const students = await studentList();
+    res
+      .status(200)
+      .json({ message: "Successfully retrieve all students", data: students });
   } catch (e) {
     next(e);
   }
 };
-const createNewStudent = async () => {};
-const getSingleStudent = async () => {};
-const updateStudent = async () => {};
-const deleteStudent = async () => {};
-const getAllStudentCourse = async () => {};
-const getAllStudentMajor = async () => {};
+const createNewStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const student = await addStudent(req.body);
+    res
+      .status(201)
+      .json({ message: "Successfully created new student", data: student });
+  } catch (e) {
+    next(e);
+  }
+};
+const getSingleStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const info = await studentInfo(id);
+    res.status(200).json({
+      message: `Successfully get info of student with id #${id}`,
+      data: info,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+const updateStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const student = await updateInfo(req.body, id);
+    res.status(200).json({
+      message: `Successfully update info of student with id #${id}`,
+      data: student,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+const deleteStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const student = await removeInfo(id);
+    res.status(200).json({
+      message: `Successfully remove info of student with id #${id}`,
+      data: student,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+const enrollCourse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { id } = req.params;
+  const { course_id } = req.body;
+  try {
+    const enrollment = await registerCourse(id, course_id);
+    res.status(201).json({
+      message: `Successfully enrolled student with id #${id} into course with id #${course_id}`,
+      data: enrollment,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+const getAllStudentCourse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const course = await getCourse(id);
+    res.status(200).json({
+      message: `Successfully get all courses that student with id #${id} enrolled`,
+      data: course,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+const getAllStudentMajor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {};
 
 export {
   getAllStudent,
@@ -32,6 +132,7 @@ export {
   getSingleStudent,
   updateStudent,
   deleteStudent,
+  enrollCourse,
   getAllStudentCourse,
   getAllStudentMajor,
 };
