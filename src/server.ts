edb761d37679f9a7e5c9c4fa100636a0ca926498
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-23 19:20:54 +1100

    On shutdown server, end pool connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import app from "./app";
 import { initDb } from "./database/init";
+import { pool } from "./database/pool";
 
 // Initialise database on start-up
 initDb();
 
 const PORT = 3004;
-app.listen(PORT);
+const server = app.listen(PORT);
+
+process.on("SIGINT", async () => {
+  await pool.end();
+  server.close(() => process.exit(0));
+});
