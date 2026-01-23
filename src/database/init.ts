a0207db4031da5089607d2430d35506f4c2c4b22
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-01-23 19:04:13 +1100

    Remove try/catch block to fix linting errors

--- src/database/init.ts
+++ src/database/init.ts
@@ -2,10 +2,9 @@ import { pool } from "./pool";
 
 // Set up database schema
 // Link to ER Diagram - https://lucid.app/lucidchart/58de6dd1-7030-4c6c-abe0-e53aaa9c1b43/edit?viewport_loc=-1235%2C-456%2C2175%2C1041%2C0_0&invitationId=inv_39190bf5-33fd-4512-80eb-05ed6e48241a
-export const initDb = async (): Promise<void> | never => {
-  try {
-    // Create table for students
-    await pool.query(`
+export const initDb = async (): Promise<void> => {
+  // Create table for students
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS students (
         student_id SERIAL PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
@@ -14,15 +13,15 @@ export const initDb = async (): Promise<void> | never => {
         gender VARCHAR(1) NOT NULL CHECK (gender IN ('M','F'))
       );  
     `);
-    // Create table for departments
-    await pool.query(`
+  // Create table for departments
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS departments (
         department_id SERIAL PRIMARY KEY,
         department_name VARCHAR(255) NOT NULL
       );  
     `);
-    // Create table for courses
-    await pool.query(`
+  // Create table for courses
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS courses (
         course_id SERIAL PRIMARY KEY,
         course_title VARCHAR(255) NOT NULL,
@@ -31,8 +30,8 @@ export const initDb = async (): Promise<void> | never => {
         department_id INTEGER REFERENCES departments(department_id) NOT NULL
       );  
     `);
-    // Create table for instructors
-    await pool.query(`
+  // Create table for instructors
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS instructors (
         instructor_id SERIAL PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
@@ -42,15 +41,15 @@ export const initDb = async (): Promise<void> | never => {
         department_id INTEGER REFERENCES departments(department_id) NOT NULL
       );  
     `);
-    // Create table for majors
-    await pool.query(`
+  // Create table for majors
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS majors (
         major_id SERIAL PRIMARY KEY,
         major_name VARCHAR(255) NOT NULL
       );  
     `);
-    // Create junction table for students-courses
-    await pool.query(`
+  // Create junction table for students-courses
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS enrollments (
         enrollment_id SERIAL PRIMARY KEY,
         student_id INTEGER NOT NULL REFERENCES students(student_id) ON DELETE CASCADE,
@@ -58,8 +57,8 @@ export const initDb = async (): Promise<void> | never => {
         UNIQUE(student_id, course_id)
       );  
     `);
-    // Create junction table for courses-majors
-    await pool.query(`
+  // Create junction table for courses-majors
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS course_major (
         course_major_id SERIAL PRIMARY KEY,
         course_id INTEGER NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
@@ -67,8 +66,8 @@ export const initDb = async (): Promise<void> | never => {
         UNIQUE(course_id, major_id)
       );  
     `);
-    // Create junction table for students-majors
-    await pool.query(`
+  // Create junction table for students-majors
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS student_major (
         student_major_id SERIAL PRIMARY KEY,
         student_id INTEGER NOT NULL REFERENCES students(student_id) ON DELETE CASCADE,
@@ -76,8 +75,8 @@ export const initDb = async (): Promise<void> | never => {
         UNIQUE(student_id, major_id)
       );  
     `);
-    // Create junction table for courses-instructors
-    await pool.query(`
+  // Create junction table for courses-instructors
+  await pool.query(`
       CREATE TABLE IF NOT EXISTS course_instructor (
         course_instructor_id SERIAL PRIMARY KEY,
         course_id INTEGER NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
@@ -85,7 +84,4 @@ export const initDb = async (): Promise<void> | never => {
         UNIQUE(course_id, instructor_id)
       );  
     `);
-  } catch (e) {
-    throw e;
-  }
 };
--- src/database/update.ts
+++ src/database/update.ts
@@ -1,11 +1,7 @@
 import { pool } from "./pool";
 
-export const updateDb = async (): Promise<void> | never => {
-  try {
-    await pool.query(
-      "ALTER TABLE majors ADD IF NOT EXISTS department_id INTEGER NOT NULL REFERENCES departments(department_id)",
-    );
-  } catch (e) {
-    throw e;
-  }
+export const updateDb = async (): Promise<void> => {
+  await pool.query(
+    "ALTER TABLE majors ADD IF NOT EXISTS department_id INTEGER NOT NULL REFERENCES departments(department_id)",
+  );
 };
