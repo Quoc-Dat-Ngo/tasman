99b3ce584c57780a7a6774f2b03a3b85ffff325b
Kevin Ngo <quocdatngo2003@gmail.com>
2026-01-30 22:53:24 +1100

    Add helper function for updateCourse service function

--- src/utils/updateTable.ts
+++ src/utils/updateTable.ts
@@ -1,15 +1,21 @@
-import type { UpdateStudentDTO } from "../types";
+import type { UpdateCourseDTO, UpdateStudentDTO } from "../types";
 
-interface updateTableReturnType {
+interface updateStudentTableReturnType {
   fields: string[];
   values: (string | Date | string[] | undefined)[];
   index: number;
 }
 
-export const updateTable = (
+interface updateCourseTableReturnType {
+  fields: string[];
+  values: (string | number | string[] | undefined)[];
+  index: number;
+}
+
+export const updateStudentTable = (
   body: UpdateStudentDTO,
   id: string | string[] | undefined,
-): updateTableReturnType => {
+): updateStudentTableReturnType => {
   const fields: string[] = [];
   const values: (string | Date | string[] | undefined)[] = [];
   let index: number = 1;
@@ -29,6 +35,36 @@ export const updateTable = (
   if (body.gender !== undefined) {
     fields.push(`gender = $${index++}`);
     values.push(body.gender);
+  }
+
+  values.push(id);
+
+  return { fields, values, index };
+};
+
+export const updateCourseTable = (
+  body: UpdateCourseDTO,
+  id: string | string[] | undefined,
+): updateCourseTableReturnType => {
+  const fields: string[] = [];
+  const values: (string | number | string[] | undefined)[] = [];
+  let index: number = 1;
+
+  if (body.course_title !== undefined) {
+    fields.push(`course_title = $${index++}`);
+    values.push(body.course_title);
+  }
+  if (body.course_code !== undefined) {
+    fields.push(`course_code = $${index++}`);
+    values.push(body.course_code);
+  }
+  if (body.fee !== undefined) {
+    fields.push(`fee = $${index++}`);
+    values.push(body.fee);
+  }
+  if (body.department_id !== undefined) {
+    fields.push(`department_id = $${index++}`);
+    values.push(body.department_id);
   }
 
   values.push(id);
