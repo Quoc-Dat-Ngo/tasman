64df106892cb6df529a2909b1a9adb77ffa3fe54
Quoc Dat(Kevin) Ngo <quocdatngo2003@gmail.com>
2026-02-04 12:19:09 +1100

    Specify pg types so that only the date will be returned without timezone/timestamp

--- src/database/pool.ts
+++ src/database/pool.ts
@@ -1,5 +1,9 @@
 import "dotenv/config";
-import { Pool } from "pg";
+import { Pool, types } from "pg";
+
+// 1082 is the OID for the PostgreSQL 'DATE' type
+// This tells node-postgres to return the raw string from the DB
+types.setTypeParser(1082, (val) => val);
 
 export const pool: Pool = new Pool({
   connectionString: process.env.DATABASE_URL + "&sslmode=verify-full",
