3b6ecfdf8980da9fe2f5c7f5d6686af34f872d70
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-16 22:37:46 +1100

    Refactor course services function to enhance scalability + readability

---
+++ src/services/course.services.ts
@@ -0,0 +1,74 @@
+import { assertFound } from "../errors/assertFound";
+import { parseParamID } from "../http/parseParamID";
+import { PoolCourseRepo } from "../repositories/course.repositories";
+import type {
+  CreateCourseDTO,
+  BaseQuery,
+  UpdateCourseDTO,
+  ExpressParamID,
+} from "../types";
+
+const repo = new PoolCourseRepo();
+
+const getAllCourseService = async (query: BaseQuery) => {
+  return repo.getAll(query);
+};
+const createNewCourseService = async (data: CreateCourseDTO) => {
+  return repo.create(data);
+};
+const getSingleCourseService = async (id: ExpressParamID) => {
+  const course_id = parseParamID(id);
+  return assertFound(
+    repo.getOne(course_id),
+    `Course with id ${course_id} not found`,
+  );
+};
+const updateCourseService = async (
+  id: ExpressParamID,
+  data: UpdateCourseDTO,
+) => {
+  const course_id = parseParamID(id);
+  return assertFound(
+    repo.update(course_id, data),
+    `Course with id ${course_id} not found`,
+  );
+};
+const deleteCourseService = async (id: ExpressParamID) => {
+  const course_id = parseParamID(id);
+  return assertFound(
+    repo.delete(course_id),
+    `Course with id ${course_id} not found`,
+  );
+};
+const getCourseStudentService = async (id: ExpressParamID) => {
+  const course_id = parseParamID(id);
+  return assertFound(
+    repo.getStudent(course_id),
+    `Course with id ${course_id} not found`,
+  );
+};
+const getCourseInstructorService = async (id: ExpressParamID) => {
+  const course_id = parseParamID(id);
+  return assertFound(
+    repo.getInstructor(course_id),
+    `Course with id ${course_id} not found`,
+  );
+};
+const getCourseDepartmentService = async (id: ExpressParamID) => {
+  const course_id = parseParamID(id);
+  return assertFound(
+    repo.getDepartment(course_id),
+    `Course with id ${course_id} not found`,
+  );
+};
+
+export {
+  getAllCourseService,
+  createNewCourseService,
+  getSingleCourseService,
+  updateCourseService,
+  deleteCourseService,
+  getCourseStudentService,
+  getCourseInstructorService,
+  getCourseDepartmentService,
+};
