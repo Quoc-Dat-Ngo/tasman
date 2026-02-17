6b161184a7ea2fb5adeca13d4f202154f82b02aa
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-17 15:24:30 +1100

    Rename controller functions

--- src/routes/enrollments.routes.ts
+++ src/routes/enrollments.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { enrollCourseController } from "../controllers/students/enrollment.controllers";
+import {
+  enrollCourseController,
+  removeEnrollCourseController,
+} from "../controllers/students/enrollment.controllers";
 export const enrollmentRouter = Router();
 
 enrollmentRouter.route("/").post(enrollCourseController);
-enrollmentRouter.route("/:id").delete(); // TODO
+enrollmentRouter.route("/:id").delete(removeEnrollCourseController);
