9ca6769f48315eb7cae84437ef91a9d5b823714f
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-17 13:52:52 +1100

    Rename controller function names

--- src/routes/majors.routes.ts
+++ src/routes/majors.routes.ts
@@ -3,20 +3,23 @@ import type { Router } from "express";
 export const majorsRouter: Router = express.Router();
 
 import {
-  getAllMajor,
-  createNewMajor,
-  getSingleMajor,
-  updateMajor,
-  deleteMajor,
-  getAllMajorStudent,
-  getMajorDepartment,
-} from "../controllers/majorsController";
+  getAllMajorController,
+  createNewMajorController,
+  getSingleMajorController,
+  updateMajorController,
+  deleteMajorController,
+  getMajorStudentController,
+  getMajorDepartmentController,
+} from "../controllers/major.controllers";
 
-majorsRouter.route("/").get(getAllMajor).post(createNewMajor);
+majorsRouter
+  .route("/")
+  .get(getAllMajorController)
+  .post(createNewMajorController);
 majorsRouter
   .route("/:id")
-  .get(getSingleMajor)
-  .patch(updateMajor)
-  .delete(deleteMajor);
-majorsRouter.route("/:id/students").get(getAllMajorStudent);
-majorsRouter.route("/:id/department").get(getMajorDepartment);
+  .get(getSingleMajorController)
+  .patch(updateMajorController)
+  .delete(deleteMajorController);
+majorsRouter.route("/:id/students").get(getMajorStudentController);
+majorsRouter.route("/:id/department").get(getMajorDepartmentController);
