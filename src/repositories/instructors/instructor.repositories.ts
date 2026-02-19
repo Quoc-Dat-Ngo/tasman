ee881b5d54a814aad488ac017365d4d172c8d895
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-19 12:52:55 +1100

    Add repository layer to interact with db independently

---
+++ src/repositories/instructors/instructor.repositories.ts
@@ -0,0 +1,146 @@
+import { pool } from "../../database/pool";
+import type {
+  BaseQuery,
+  Course,
+  CreateInstructorDTO,
+  Department,
+  Instructor,
+  UpdateInstructorDTO,
+} from "../../types/index.types";
+import type { EntityRepository } from "../EntityRepository.interface";
+import { updateQueryBuilder } from "../helpers/updateQueryBuilder";
+
+interface InstructorRepository extends EntityRepository<
+  Instructor,
+  Instructor[],
+  CreateInstructorDTO,
+  UpdateInstructorDTO,
+  BaseQuery
+> {
+  getCourse(id: string): Promise<Course[] | null>;
+  getDepartment(id: string): Promise<Department | null>;
+}
+
+export class PoolInstructorRepo implements InstructorRepository {
+  async getAll(query?: BaseQuery | undefined): Promise<Instructor[]> {
+    const { limit = 10, offset = 0, sort } = query!;
+    let index = 1;
+    return (
+      await pool.query<Instructor>(
+        `
+          SELECT * 
+          FROM instructors
+          LIMIT $${index++}
+          OFFSET $${index}
+        `,
+        [limit, offset],
+      )
+    ).rows;
+  }
+
+  async getOne(id: string): Promise<Instructor | null> {
+    return (
+      (
+        await pool.query<Instructor>(
+          `
+            SELECT * 
+            FROM instructors
+            WHERE instructor_id = $1
+          `,
+          [id],
+        )
+      ).rows[0] ?? null
+    );
+  }
+
+  async create(data: CreateInstructorDTO): Promise<Instructor> {
+    return (
+      await pool.query<Instructor>(
+        `
+            INSERT INTO instructors (first_name, last_name, dob, gender, department_id)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *;
+          `,
+        [
+          data.first_name,
+          data.last_name,
+          data.dob,
+          data.gender,
+          data.department_id,
+        ],
+      )
+    ).rows[0]!;
+  }
+
+  async update(
+    id: string,
+    data: UpdateInstructorDTO,
+  ): Promise<Instructor | null> {
+    const allowedFields: (keyof UpdateInstructorDTO)[] = [
+      "first_name",
+      "last_name",
+      "dob",
+      "gender",
+      "department_id",
+    ];
+    const { setString, values } = updateQueryBuilder(data, allowedFields, id);
+    return (
+      (
+        await pool.query<Instructor>(
+          `
+        UPDATE instructors
+        SET ${setString}
+        WHERE instructor_id = $${values.length}
+        RETURNING *;
+      `,
+        )
+      ).rows[0] ?? null
+    );
+  }
+
+  async delete(id: string): Promise<Instructor | null> {
+    return (
+      (
+        await pool.query<Instructor>(
+          `
+            DELETE FROM instructors
+            WHERE instructor_id = $1
+          `,
+          [id],
+        )
+      ).rows[0] ?? null
+    );
+  }
+
+  async getCourse(id: string): Promise<Course[] | null> {
+    const result = await pool.query<Course>(
+      `
+        SELECT c.course_title, c.course_code
+        FROM courses as c
+        JOIN course_instructor as ci
+        ON ci.course_id = c.course_id
+        WHERE ci.instructor_id = $1
+      `,
+      [id],
+    );
+
+    return result.rows.length ? result.rows : null;
+  }
+
+  async getDepartment(id: string): Promise<Department | null> {
+    return (
+      (
+        await pool.query<Department>(
+          `
+            SELECT d.department_name 
+            FROM departments as d
+            JOIN instructors as i 
+            ON i.department_id = d.department_id
+            WHERE i.instructor_id = $1
+          `,
+          [id],
+        )
+      ).rows[0] ?? null
+    );
+  }
+}
---
+++ src/repositories/instructors/instructorCourse.repositories.ts
@@ -0,0 +1,38 @@
+import { pool } from "../../database/pool";
+import type { InstructorCourse } from "../../types/index.types";
+
+interface InstructorCourseRepository {
+  register(
+    courseId: string,
+    instructorID: string,
+  ): Promise<InstructorCourse | null>;
+  delete(id: string): Promise<InstructorCourse | null>;
+}
+export class PoolInstructorCourseRepo implements InstructorCourseRepository {
+  async register(
+    courseId: string,
+    instructorID: string,
+  ): Promise<InstructorCourse | null> {
+    const result = await pool.query<InstructorCourse>(
+      `
+        INSERT INTO course_instructor (course_id, instructor_id)
+        VALUES ($1, $2)
+        RETURNING *;
+      `,
+      [courseId, instructorID],
+    );
+    return result.rows[0] ?? null;
+  }
+
+  async delete(id: string): Promise<InstructorCourse | null> {
+    const result = await pool.query<InstructorCourse>(
+      `
+        DELETE FROM course_instructor
+        WHERE course_instructor_id = $1
+        RETURNING *;
+      `,
+      [id],
+    );
+    return result.rows[0] ?? null;
+  }
+}
