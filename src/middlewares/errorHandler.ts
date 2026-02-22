0c36dddc46b9e270dec6979524ca7bf2a6ed90d6
Kevin Ngo <quocdatngo2003@gmail.com>
2026-02-22 17:43:12 +1100

    Mock entity data with CSV files using Postman feature of bulk run(insert)

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -19,7 +19,7 @@ export const globalErrorHandler = (
     // Unexpected error
     res.status(500).json({
       status: "error",
-      message: "Internal Server Error",
+      message: err,
     });
   }
 };
