a048e7767b611546a11a81ce663103586f89b82c
Kevin Ngo <quocdatngo2003@gmail.com>
2026-03-03 21:52:57 +1100

    Define Zod validation schema for .env file inputs; Validate .env file inputs

---
+++ src/config/env.schema.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const Env = z.object({
+  DATABASE_URL: z.string().trim().min(1),
+  PORT: z.coerce.number().int().min(1).max(65535).default(3004),
+  ACCESS_TOKEN_SECRET_KEY: z.string().trim().min(32),
+  REFRESH_TOKEN_SECRET_KEY: z.string().trim().min(32),
+});
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,24 +1,11 @@
 import "dotenv/config";
+import { Env } from "./env.schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("Database URL connection string is not defined");
-}
-
-if (!process.env.PORT) {
-  console.warn("PORT is not defined, use default port - 3004");
-}
-
-if (!process.env.ACCESS_TOKEN_SECRET_KEY) {
-  throw new Error("Access Token secret key is not defined");
-}
-
-if (!process.env.REFRESH_TOKEN_SECRET_KEY) {
-  throw new Error("Refresh Token secret key is not defined");
-}
+const validatedEnv = Env.parse(process.env);
 
 export const env = {
-  DATABASE_URL: process.env.DATABASE_URL + "&sslmode=verify-full",
-  PORT: process.env.PORT ?? 3004,
-  ACCESS_KEY: process.env.ACCESS_TOKEN_SECRET_KEY,
-  REFRESH_KEY: process.env.REFRESH_TOKEN_SECRET_KEY,
+  DATABASE_URL: validatedEnv.DATABASE_URL,
+  PORT: validatedEnv.PORT,
+  ACCESS_KEY: validatedEnv.ACCESS_TOKEN_SECRET_KEY,
+  REFRESH_KEY: validatedEnv.REFRESH_TOKEN_SECRET_KEY,
 };
