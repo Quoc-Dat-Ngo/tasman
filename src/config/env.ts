6923c39123c1027e193d031eac9bd9553f9eb3a2
Kevin Ngo <quocdatngo2003@gmail.com>
2026-03-05 14:56:46 +1100

    Define Zod schema for .env input values

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { Env } from "./env.schema";
+import { EnvSchema } from "./env.schema";
 
-const validatedEnv = Env.parse(process.env);
+const validatedEnv = EnvSchema.parse(process.env);
 
 export const env = {
   DATABASE_URL: validatedEnv.DATABASE_URL,
