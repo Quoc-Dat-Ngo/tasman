ccd38c52a9e3017fce067b6d487741c0a9a838a6
Kevin Ngo <quocdatngo2003@gmail.com>
2026-03-13 18:51:55 +1100

    Define schema for courses' input data

---
+++ src/schema/course.schemas.ts
@@ -0,0 +1,25 @@
+import * as z from "zod";
+import {
+  IDSchema,
+  ParamsSchema,
+  PaginationQuerySchema,
+} from "./common.schemas";
+
+export { ParamsSchema, PaginationQuerySchema };
+
+const CourseBaseSchema = z.object({
+  course_title: z.string().trim().min(1),
+  course_code: z.string().trim().min(1),
+  fee: z.coerce.number().positive(),
+  department_id: z.coerce.number().int().positive(),
+});
+
+export const CreateCourseSchema = CourseBaseSchema;
+
+export const UpdateCourseSchema = CourseBaseSchema.partial().refine(
+  (data) => Object.keys(data).length > 0,
+  { message: "At least one field must be updated" },
+);
+
+export const CourseQuerySchema = PaginationQuerySchema;
+
