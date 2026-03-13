d7e25a5ab0fcb087457632cbfb5610a8be3576b7
Kevin Ngo <quocdatngo2003@gmail.com>
2026-03-13 18:52:57 +1100

    Define schema for instructors' input data

---
+++ src/schema/instructor.schemas.ts
@@ -0,0 +1,26 @@
+import * as z from "zod";
+import {
+  IDSchema,
+  ParamsSchema,
+  PaginationQuerySchema,
+} from "./common.schemas";
+
+export { ParamsSchema, PaginationQuerySchema };
+
+const InstructorBaseSchema = z.object({
+  first_name: z.string().trim().min(1),
+  last_name: z.string().trim().min(1),
+  dob: z.coerce.date(),
+  gender: z.enum(["M", "F"]),
+  department_id: z.coerce.number().int().positive(),
+});
+
+export const CreateInstructorSchema = InstructorBaseSchema;
+
+export const UpdateInstructorSchema = InstructorBaseSchema.partial().refine(
+  (data) => Object.keys(data).length > 0,
+  { message: "At least one field must be updated" },
+);
+
+export const InstructorQuerySchema = PaginationQuerySchema;
+
